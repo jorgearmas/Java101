05eb301db4864ec00e5d7b5124892b9c8df26446
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-06 14:00:13 -0600

    null values

---
+++ src/NullValues_11.java
@@ -0,0 +1,15 @@
+public class NullValues_11 {
+    public static void main(String[] args) {
+
+        //Null values indicates the absence of a value
+        String nullString = null;
+        int intValue = 0;
+
+        System.out.println("'nullString': "+nullString);
+        System.out.println("'intValue': "+intValue);
+
+        //Blank values
+        String blankString = "";
+        System.out.println("'blankString': "+blankString);
+    }
+}
