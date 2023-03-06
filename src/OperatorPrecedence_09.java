6a64dba90a92ba9d971acbabd907f47ea1b6b2b5
Jorge Armas <jorgearmasc0496@gmail.com>
2023-03-06 11:58:22 -0600

    Math operator precedence

---
+++ src/OperatorPrecedence_09.java
@@ -0,0 +1,15 @@
+public class OperatorPrecedence_09 {
+    public static void main(String[] args) {
+
+        double a = 2, b = 5, c = 6, d = 10;
+
+        System.out.println("a+b*d = "+(a+b*d));
+        System.out.println("(a+b)*d = "+(a+b)*d);
+        System.out.println("a+(b*d) = "+(a+(b*d)));
+
+        System.out.println("\na+b/d = "+(a+b/d));
+        System.out.println("a+b/d = "+((a+b)/d));
+        System.out.println("a+b/c-d = "+(a+b/c-d));
+        System.out.println("a+b/(c-d) = "+(a+b/c-d));
+    }
+}
